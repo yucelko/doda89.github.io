e715bedc6772281f5fa89fbf12bef6f145de3a53
Yucel <yucelkolcubasi@gmail.com>
2020-09-26 18:59:03 -0400

    sfa

--- gamble/controller.js
+++ gamble/controller.js
@@ -3,8 +3,8 @@ var flag = "23-30Dec";
 function bringMatches() {
   alert(document.getElementById('startDate').value);
   alert(document.getElementById('endDate').value);
-  //myFunk(document.getElementById('startDate').value);
-//  myFunk(document.getElementById('endDate').value);
+  myFunk(document.getElementById('startDate').value);
+  myFunk(document.getElementById('endDate').value);
 }
 $(document).ready(function() {
   $("input[type=checkbox]").change("click", doit);
